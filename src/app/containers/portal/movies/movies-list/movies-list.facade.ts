187e75777b1228ddf8f854301ac1a349a4078a2d
Roman Holyk <romangolik86786@gmail.com>
2023-04-20 20:08:33 +0300

    Created movies-page facade

--- src/app/containers/portal/movies/movies-list/movies-list.facade.ts
+++ src/app/containers/portal/movies/movies-list/movies-list.facade.ts
@@ -6,7 +6,6 @@ import { map, switchMap } from 'rxjs/operators';
 
 import { MoviesService } from '@rest/movies/movies.service';
 import { GenresService } from '@rest/genres/genres.service';
-import { TvShowsService } from '@rest/tv-shows/tv-shows.service';
 
 import { MediaDto } from '@rest/media/_types/media.dto';
 
@@ -15,7 +14,6 @@ export class HomeFacade {
   constructor(
     private moviesService: MoviesService,
     private genresService: GenresService,
-    private tvShowsService: TvShowsService
   ) {}
 
   getTrendingMovies(params?: Params): Observable<MediaDto[]> {
@@ -33,16 +31,6 @@ export class HomeFacade {
           })))
         )
       )
-    );
-  }
-
-  getTrendingTvShows(params?: Params): Observable<MediaDto[]> {
-    return this.tvShowsService.getTrending({
-      'language': 'en-US',
-      'page': '1',
-      ...params
-    }).pipe(
-      map(data => data.results)
     );
   }
 }
